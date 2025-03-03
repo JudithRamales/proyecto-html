98f68534151b569abe618f40e79ced3d2f129e24
judith <89990755+JudithRamales@users.noreply.github.com>
2025-03-03 12:37:11 -0600

    creacion de ejercicio

---
+++ 5.py
@@ -0,0 +1,10 @@
+"""HAZ UN PORGRAMA QUE PIDA
+LA EDAD DEL USARIO Y
+DIGA SI ES MAYOR DE EDAD O NO"""
+
+edad = input("Edad: ")
+
+if int(edad) >= 18:
+    print("Eres mayor de edad")
+else:
+    print("No eres mayor de edad")
